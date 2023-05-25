a9fca128b5a0a60ace565842e194c4a3edc2d856
statbuster <richardlevinson552@gmail.com>
2023-05-25 07:19:58 -0800

    feat: EDA

--- main.r
+++ main.r
@@ -93,3 +93,29 @@ p8 <- ggplot(combi |> group_by(Outlet_Type) |> summarise(count=n()))
     + theme(axis.text.x = element_text(size=8.5))
 
 plot_grid(p7, p8, ncol=2)
+
+# Target Variable vs Independent Numerical Variables
+
+p9 <- ggplot(train)  + geom_point(aes(Item_Weight, Item_Outlet_Sales), color = "violet", alpha = 0.5) + theme(axis.title = element_text(size = 8.5))
+
+p10 <- ggplot(train)  + geom_point(aes(Item_Visibility, Item_Outlet_Sales), color = "violet", alpha = 0.5)  + theme(axis.title = element_text(size = 8.5))
+
+p11 <- ggplot(train) + geom_point(aes(Item_MRP, Item_Outlet_Sales), color = "violet", alpha = 0.5)  + theme(axis.title = element_text(size = 8.5))
+
+second_row_2 <- plot_grid(p10, p11, ncol = 2)
+plot_grid(p9, second_row_2, nrow=2)
+
+
+p12 <- ggplot(train) +  geom_violin(aes(Item_Type, Item_Outlet_Sales), fill = "magenta") + theme(axis.text.x = element_text(angle = 45, hjust = 1),  axis.text = element_text(size = 6),  axis.title = element_text(size = 8.5))
+p13 <- ggplot(train) +  geom_violin(aes(Item_Fat_Content, Item_Outlet_Sales), fill = "magenta") + theme(axis.text.x = element_text(angle = 45, hjust = 1),  axis.text = element_text(size = 8),  axis.title = element_text(size = 8.5))
+p14 <- ggplot(train) +  geom_violin(aes(Outlet_Identifier, Item_Outlet_Sales), fill = "magenta") + theme(axis.text.x = element_text(angle = 45, hjust = 1),  axis.text = element_text(size = 8), axis.title = element_text(size = 8.5))
+
+second_row_3 <- plot_grid(p13, p14, ncol = 2)
+plot_grid(p12, second_row_3, nrow=2)
+
+ggplot(train) + geom_violin(aes(Outlet_Size, Item_Outlet_Sales), fill = "magenta")
+
+p15 <- ggplot(train) + geom_violin(aes(Outlet_Location_Type, Item_Outlet_Sales), fill = "magenta")
+p16 <- ggplot(train) + geom_violin(aes(Outlet_Type, Item_Outlet_Sales), fill = "magenta")
+
+plot_grid(p15,p16,ncol=1)
