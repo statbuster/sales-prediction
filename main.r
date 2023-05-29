e7816dcaab15b934eca54d851c72b8c53ff23600
statbuster <richardlevinson552@gmail.com>
2023-05-29 07:43:58 -0800

    feat: xgboost model

--- main.r
+++ main.r
@@ -199,9 +199,52 @@ control <- trainControl(method='cv', number=5)
 Grid <- expand.grid(alpha=1, lambda=seq(0.001,0.1,by=0.0002))
 lasso_linear_reg_model <- train(x = train[,-c("Item_Identifier")], y = train$Item_Outlet_Sales, method = "glmnet", trControl = control, tuneGrid = Grid)
 
-# Ridge Regression
+# Ridge Regression 1219.08
 
 set.seed(1235)
 control <- trainControl(method='cv', number=5)
 Grid <- expand.grid(alpha=0, lambda=seq(0.001,0.1,by=0.0002))
 ridge_linear_reg_model <- train(x = train[,-c("Item_Identifier")], y = train$Item_Outlet_Sales, method = "glmnet", trControl = control, tuneGrid = Grid)
+
+
+# RandomForest
+
+set.seed(1237)
+control <- trainControl(method = "cv", number = 5)
+tgrid <- expand.grid( mtry = c(3:10), spliturle = "variance", min.node.size = c(10,15,20))
+rf_mod <- train(x=train[,-c("Item_Identifier")], y=train$Item_Outlet_Sales, method="ranger", trControl=control, tuneGrid=tgrid, num.trees=400, importance="permutation")
+
+plot(rf_mod)
+plot(varImp(rf_mod))
+
+# XGBoost
+
+param_list <- list(
+    objective = "reg:linear",
+    eta = 0.10,
+    gamma = 1,
+    max_depth = 6,
+    subsample = 0.8,
+    colsample_bytree = 0.5
+)
+
+dtrain <- xgb.DMatrix(data = as.matrix(train[,-c("Item_Identifier")]), label = train$Item_Outlet_Sales)
+dtest <- xgb.DMatrix(data = as.matrix(test[,-c("Item_Identifier")]))
+
+set.seed(112)
+xgbcv <- xgb.cv(
+    params = param_list,
+    data = dtrain,
+    nfold = 5,
+    nrounds = 1000,
+    early_stopping_rounds = 30,
+    maximize = F
+)
+
+xgb_model <- xgb.train(
+    params = param_list,
+    data = dtrain,
+    nrounds = xgbcv$best_iteration
+)
+
+var_imp <- xgb.importance(feature_names = setdiff(names(train),c("Item_Identifier","Item_Outlet_Sales")), model = xgb_model)
